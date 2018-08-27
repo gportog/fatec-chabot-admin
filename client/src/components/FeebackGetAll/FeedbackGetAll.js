bccb89a462d306844622a08e031027ae694c1b8a
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2018-08-27 08:58:01 -0300

    Add FeedbackGetAll component :heavy_plus_sign:
    This component is responsible to get all feedback entries from our
    database and render everything into a table for search data.
    
    Signed-off-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

---
+++ client/src/components/FeebackGetAll/FeedbackGetAll.js
@@ -0,0 +1,52 @@
+import React from "react";
+import FeedbackService from '../../services/Feedback';
+import loading from '../../assets/loading.gif';
+import FeedbackGetAllTable from './FeedbackGetAllTable';
+import './FeedbackGetAll.css';
+
+class FeedbackGetAll extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            obj: [],
+            isLoading: true,
+            gotError: false,
+            error: ''
+        }
+    }
+
+    componentDidMount() {
+        FeedbackService.getAll()
+            .then((data) => {
+                return this.setState({
+                    obj: data,
+                    isLoading: false
+                })
+            })
+            .catch((err) => {
+                return this.setState({
+                    gotError: true,
+                    error: err,
+                    isLoading: false
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Feedback</h1>
+                {this.state.isLoading ? 
+                    <img src={loading} id="loading" 
+                     alt="Carregando" title="Carregando"/> 
+                    : this.state.gotError ?
+                        <p id="error">{this.state.error}</p>
+                        :
+                        <FeedbackGetAllTable obj={this.state.obj} />
+                }
+            </div>
+        )
+    }
+}
+
+export default FeedbackGetAll;
